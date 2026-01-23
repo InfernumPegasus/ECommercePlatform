0432e8ca438bf74efc5cef5bb2fffe212e32c01a
Vladimir Kirillov <vkirillov505@gmail.com>
2026-01-23 14:33:49 +0300

    Removed `HandlerSignature` type declaration

--- common/http/include/HttpCommon.hpp
+++ common/http/include/HttpCommon.hpp
@@ -5,7 +5,6 @@
 
 namespace http_common {
 
-using HandlerSignature = Response(const RequestContext&);
 template <typename Controller>
 using HandlerPtr = Response (Controller::*)(const RequestContext&);
 
